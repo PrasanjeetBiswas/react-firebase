967b77d354addbe2e9d2c9c134ab7e988f37a51c
Prasanjeet Biswas <biswaspro2@gmail.com>
2025-10-16 16:43:59 +0530

    Your commit message

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -37,7 +37,7 @@ function SignUp() {
           onChange={(e) => {
             setEmail(e.target.value);
           }}
-          style={{ padding: "10px",backgroundColor:"#353535c5",color:"#ffffff3c" }}
+          style={{ padding: "10px",backgroundColor:"#353535c5",color:"#ffffff3c",border:"none",borderRadius:"8px"  }}
         />
         <input
           type="password"
@@ -45,7 +45,7 @@ function SignUp() {
           onChange={(e) => {
             setPassword(e.target.value);
           }}
-          style={{ padding: "10px",backgroundColor:"#353535c5",color:"#ffffff3c" }}
+          style={{ padding: "10px",backgroundColor:"#353535c5",color:"#ffffff3c",border:"none",borderRadius:"8px"  }}
         />
         <button type="submit" style={{backgroundColor:"#353535c5",color:"#ffffff3c"}}>Submit</button>
       </form>
