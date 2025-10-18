e4394566e9afa71af672f1eaedf09bbe918280d0
Prasanjeet Biswas <biswaspro2@gmail.com>
2025-10-18 13:47:33 +0530

    Your commit message

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,8 @@ import {
   FacebookAuthProvider,
   TwitterAuthProvider,
   GithubAuthProvider,
+  signInWithPhoneNumber,
+  RecaptchaVerifier,
 } from "firebase/auth";
 import { app } from "../Firebase";
 import { useNavigate } from "react-router-dom";
@@ -15,6 +17,9 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [moblie, setMoblie] = useState(0);
+  const [isOtpSent, setIsOtpSent] = useState(false);
+  const [code,setCode] = useState(0);
 
   const navigate = useNavigate();
   const auth = getAuth(app);
@@ -96,6 +101,27 @@ function Login() {
       });
   };
 
+  const sendOtp=()=>{
+    const appVerifier = new RecaptchaVerifier(auth,'abc',{"size":"invisible"});
+    signInWithPhoneNumber(auth,moblie,appVerifier)
+    .then(res=>{
+          console.log("otp sent");
+          console.log(res);
+          window.confirmationResult = res;
+          setIsOtpSent(true);
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  const confirmoTP =()=>{
+    window.confirmationResult.confirm(code).then(res=>{
+      console.log(res)
+      navigate("/dasboard")
+    }).catch(err=>{console.log(err)})
+  }
+
   return (
     <>
       <div className="heading">
@@ -124,19 +150,37 @@ function Login() {
         <button type="submit" style={{backgroundColor:"#353535c5",color:"#ffffff3c"}}>Submit</button>
         <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
           <button type="button" onClick={handleloginWithGoogle} style={{borderRadius:"50%", width:"50px", height:"50px", display:"flex",alignItems:"center",justifyContent:"center",backgroundColor:"#353535c5"}}>
-<i class="fa-brands fa-google" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
+<i className="fa-brands fa-google" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
         </button>
         <button type="button" onClick={handleloginWithFacebook} style={{borderRadius:"50%", width:"50px", height:"50px", display:"flex",alignItems:"center",justifyContent:"center",backgroundColor:"#353535c5"}}>
-          <i class="fa-brands fa-facebook-f" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
+          <i className="fa-brands fa-facebook-f" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
         </button>
         <button type="button" onClick={handleloginWithTwitter} style={{borderRadius:"50%", width:"50px", height:"50px", display:"flex",alignItems:"center",justifyContent:"center",backgroundColor:"#353535c5"}}>
-          <i class="fa-brands fa-x-twitter" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
+          <i className="fa-brands fa-x-twitter" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
         </button>
         <button type="button" onClick={handleloginWithGithub} style={{borderRadius:"50%", width:"50px", height:"50px", display:"flex",alignItems:"center",justifyContent:"center",backgroundColor:"#353535c5"}}>
-          <i class="fa-brands fa-github" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
+          <i className="fa-brands fa-github" style={{fontSize:"24px",color:"#ffffff3c"}}></i>
         </button>
         
         </div>
+        {
+          !isOtpSent? <div>
+            <div style={{backgroundColor:"#353535c5",borderRadius:"8px",display:"flex",alignItems:"center",justifyContent:"space-between",overflow:"hidden"}}>
+          <input type="tel" placeholder="Enter your mobile Number" onChange={(e)=>{setMoblie(e.target.value)}}
+                    style={{ padding: "10px",backgroundColor:"transparent",border:"none",outline:"none" }}></input>
+          <button type="button" onClick={sendOtp}  style={{ padding: "10px",backgroundColor:"#555555ff",border:"none" }}>Send OTP</button>
+        </div>
+        <div id="abc"></div>
+          </div>
+        :
+        <div>
+          <h3>Confirm Your Otp</h3>
+          <div style={{backgroundColor:"#353535c5",borderRadius:"8px",display:"flex",alignItems:"center",justifyContent:"space-between",overflow:"hidden"}}>
+          <input type="tel" placeholder="Confirm your otp" onChange={(e)=>{setCode(e.target.value)}}
+                    style={{ padding: "10px",backgroundColor:"transparent",border:"none",outline:"none" }}/>
+          <button type="button" onClick={confirmoTP} style={{ padding: "10px",backgroundColor:"#555555ff",border:"none" }}>Confirm</button></div>
+        </div>
+        }
       </form>
     </>
   );
